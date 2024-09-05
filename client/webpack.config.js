c2b6241f717441f20e9248214e5eb3cb3f4f0e12
Yshrish <shrishsunder444@gmail.com>
2024-09-05 15:13:26 +0530

    first commit

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,12 @@
-devServer: {
-    allowedHosts: 'all',
-    proxy: {
-      '/api': {
-        target: 'http://213.210.21.48:9000',
-        changeOrigin: true
+module.exports = {
+    devServer: {
+      allowedHosts: 'all',  // This allows access from any host
+      proxy: {
+        '/api': {
+          target: 'http://213.210.21.48:9000',  // Proxy API requests
+          changeOrigin: true
+        }
       }
     }
-  }
+  };
   
